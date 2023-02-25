45a652f07a52bf348ad67fa928a2caf59861baa0
marceloocampogit <marceloocampo1993@icloud.com>
2023-02-25 17:27:20 -0300

    Sumo botones para mas calculos

--- script.js
+++ script.js
@@ -3,16 +3,38 @@
 let h1 = document.querySelector('h1');
 let input1 = document.querySelector('#calculo1');
 let input2 = document.querySelector('#calculo2');
-let btn = document.querySelector('#btnCalcular');
 let pResultado = document.querySelector('#resultado');
+
+let btnSuma = document.querySelector('#btnSuma');
+let btnResta = document.querySelector('#btnResta');
+let btnDivide = document.querySelector('#btnDivide');
+let btnMultiplica = document.querySelector('#btnMultiplica');
 
 ///////////////// EVENTOS ////////////////////
 
-btn.addEventListener('click', calcular);
+btnSuma.addEventListener('click', calcularSuma);
+btnResta.addEventListener('click', calcularResta);
+btnDivide.addEventListener('click', calcularDivision);
+btnMultiplica.addEventListener('click', calcularMultiplicacion);
 
 ///////////////// FUNCIONES ////////////////////
 
-function calcular() {
+function calcularSuma() {
     let resultado = parseInt(input1.value) + parseInt(input2.value);
+    pResultado.innerText = "Resultado: " + resultado;
+}
+
+function calcularResta() {
+    let resultado = parseInt(input1.value) - parseInt(input2.value);
+    pResultado.innerText = "Resultado: " + resultado;
+}
+
+function calcularMultiplicacion() {
+    let resultado = parseInt(input1.value) * parseInt(input2.value);
+    pResultado.innerText = "Resultado: " + resultado;
+}
+
+function calcularDivision() {
+    let resultado = parseInt(input1.value) / parseInt(input2.value);
     pResultado.innerText = "Resultado: " + resultado;
 }
